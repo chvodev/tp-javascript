2994ecfec2de477d1a5a97368886d4f994c24b32
Carlos Hecker <chvodev@gmail.com>
2023-03-06 22:02:56 -0300

    Agregue el enunciado, resolvi el ejercicio y lo probe

--- js/5-ejercicio.js
+++ js/5-ejercicio.js
@@ -1,1 +1,5 @@
+// Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
 
+let primerNumero = parseInt(prompt("Ingrese un número"));
+let segundoNumero = parseInt(prompt("Ingrese un número"));
+document.write(primerNumero + segundoNumero);
