0b3335ace0d2db631fc71cb6fb4d3882cbbb6172
Carlos Hecker <chvodev@gmail.com>
2023-03-06 22:12:54 -0300

    Agregue el enunciado, resolvi el ejercicio y lo probe

--- js/6-ejercicio.js
+++ js/6-ejercicio.js
@@ -1,1 +1,9 @@
+// Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 
+let primerNumero = parseInt(prompt("Ingrese un número"));
+let segundoNumero = parseInt(prompt("Ingrese un número"));
+if (primerNumero > segundoNumero) {
+  document.write(`El mayor es: ${primerNumero}`);
+} else {
+  document.write(`El mayor es: ${segundoNumero}`);
+}
