4482e4281c1a2ec7a726b63978489a31e3eccc4b
Carlos Hecker <chvodev@gmail.com>
2023-03-06 21:39:51 -0300

    Agregue el enunciado, resolvi el ejercicio y lo probe

--- js/4-ejercicio.js
+++ js/4-ejercicio.js
@@ -1,1 +1,4 @@
+// Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
 
+let nombreUsuario = prompt("Escriba su nombre de usuario");
+document.write("Hola " + nombreUsuario);
