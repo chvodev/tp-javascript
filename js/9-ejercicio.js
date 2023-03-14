a4bd0bd2cfc0d2035df1ec916563ba1193cdb8b3
Carlos Hecker <chvodev@gmail.com>
2023-03-14 20:37:43 -0300

    Agregue el enunciado del ejercicio 9, lo resolvi y testie

--- js/9-ejercicio.js
+++ js/9-ejercicio.js
@@ -1,1 +1,16 @@
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 
+let frase = prompt("Ingrese una frase");
+frase = frase.toLowerCase();
+
+for (let vocal = 0; vocal < frase.length; vocal++) {
+  if (
+    frase.charAt(vocal) == "a" ||
+    frase.charAt(vocal) == "e" ||
+    frase.charAt(vocal) == "i" ||
+    frase.charAt(vocal) == "o" ||
+    frase.charAt(vocal) == "u"
+  ) {
+    document.write(frase.charAt(vocal));
+  }
+}
